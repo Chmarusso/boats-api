6d33d9f29bd17cdf88df70f18ad269e3e84fb402
Artur Chmaro <artur.chmaro@consileon.pl>
2018-11-18 19:07:04 +0100

    Basic validations

--- db/models/user.js
+++ db/models/user.js
@@ -3,7 +3,14 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true,
+        isEmail: true,
+        unique: true
+      }
+    },
     passwordHash: DataTypes.STRING
   }, {});
   User.associate = function(models) {
